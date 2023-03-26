ddfd5f6641dca4960a938c1bc80814d427169fb5
edd255 <abaddon@disroot.org>
2023-03-26 20:46:44 +0200

    refactor: added prompt_results, moved macros to header

--- src/sh.c
+++ src/sh.c
@@ -1,179 +1,162 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include "../deps/colorful-printf/colorprint.h"
+#include "deps/colorful-printf/colorprint.h"
 #include "sh.h"
 
-#define BUFSIZE 64
-#define DELIMITER " \t\r\n\a"
+//=== PARSING ==================================================================
 
-#define UNUSED(x) \
-{                 \
-        (void) x; \
-}
-
-char* USERNAME;
-char HOSTNAME[256];
-
-char* sh_read_line(void)
+static char* sh_read_line(void)
 {
-        char* line = NULL;
-        size_t bufsize = 0;
-        if (getline(&line, &bufsize, stdin) == -1) {
-                feof(stdin) ? exit(EXIT_SUCCESS) : exit(EXIT_FAILURE);
-        }
-        return line;
+	char* line = NULL;
+	size_t bufsize = 0;
+	if (getline(&line, &bufsize, stdin) == -1) {
+		feof(stdin) ? exit(EXIT_SUCCESS) : exit(EXIT_FAILURE);
+	}
+	return line;
 }
 
 
-char** sh_split_line(char* line)
+static char** sh_split_line(char* line)
 {
-        int bufsize = BUFSIZE;
-        int position = 0;
-        char** tokens = malloc(bufsize * sizeof(char*));
-        if (tokens == NULL) {
-                fprintf(stderr, "sh: allocation error\n");
-                exit(EXIT_FAILURE);
-        }
-        char* token = strtok(line, DELIMITER);
-        while (token != NULL) {
-                tokens[position] = token;
-                position++;
+	int bufsize = BUFSIZE;
+	int position = 0;
+	char** tokens = malloc(bufsize * sizeof(char*));
+	if (tokens == NULL) {
+		fprintf(stderr, "sh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	char* token = strtok(line, DELIMITER);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
 
-                if (position >= bufsize) {
-                        bufsize += BUFSIZE;
-                        tokens = realloc(tokens, bufsize * sizeof(char*));
-                        if (tokens == NULL) {
-                                fprintf(stderr, "sh: realocation error\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-                token = strtok(NULL, DELIMITER);
-        }
-        tokens[position] = NULL;
-        return tokens;
+		if (position >= bufsize) {
+			bufsize += BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (tokens == NULL) {
+				fprintf(stderr, "sh: realocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, DELIMITER);
+	}
+	tokens[position] = NULL;
+	return tokens;
 }
 
+//=== EXECUTING ================================================================
 
-int sh_launch(char** args)
+static int sh_launch(char** args)
 {
-        pid_t pid = fork();
-        if (pid == 0) {
-                // Child process
-                if (execvp(args[0], args) == -1) {
-                        perror("sh: execvp error");
-                }
-                exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-                perror("sh: pid is smaller than 0");
-                return 0;
-        }
-        int status;
-        do {
-                waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        return 1;
+	pid_t pid = fork();
+	if (pid == 0) {
+		// Child process
+		if (execvp(args[0], args) == -1) {
+			perror("sh: execvp error");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		perror("sh: pid is smaller than 0");
+		return 0;
+	}
+	int status;
+	do {
+		waitpid(pid, &status, WUNTRACED);
+	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	return 1;
 }
 
+//=== BUILTINS =================================================================
 
-char* builtins[] = {
-        "cd",
-        "help",
-        "exit"
+static char* builtins[] = {
+	"cd",
+	"help",
+	"exit"
 };
 
-
-int sh_cd(char** args) {
-        if (args[1] == NULL) {
-                fprintf(stderr, "sh: expected argument to cd");
-        } else {
-                if (chdir(args[1]) != 0) {
-                        perror("sh: cd failed");
-                }
-        }
-        return 1;
+static int sh_cd(char** args) {
+	if (args[1] == NULL) {
+		fprintf(stderr, "sh: expected argument to cd");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("sh: cd failed");
+		}
+	}
+	return 1;
 }
 
-
-int sh_help(char** args) {
-        UNUSED(args);
-        printf("SHELL\n");
-        printf("Built-ins:\n");
-        for (int i = 0; i < 3; i++) {
-                printf("%s\n", builtins[i]);
-        }
-        return 1;
+static int sh_help(char** args) {
+	UNUSED(args);
+	printf("SHELL\n");
+	printf("Built-ins:\n");
+	for (int i = 0; i < 3; i++) {
+		printf("%s\n", builtins[i]);
+	}
+	return 1;
 }
 
-
-int sh_exit(char** args)
+static int sh_exit(char** args)
 {
-        UNUSED(args);
-        return 0;
+	UNUSED(args);
+	return 0;
 }
 
-
-int (*builtins_func[]) (char**) = {
-        &sh_cd,
-        &sh_help,
-        &sh_exit
+static int (*builtins_func[]) (char**) = {
+	&sh_cd,
+	&sh_help,
+	&sh_exit
 };
 
-
-int sh_execute(char** args)
+static int sh_execute(char** args)
 {
-        if (args[0] == NULL) {
-                return 1;
-        }
-        for (int i = 0; i < 3; i++) {
-                if (strcmp(args[0], builtins[i]) == 0) {
-                        return (*builtins_func[i])(args);
-                }
-        }
-        return sh_launch(args);
+	if (args[0] == NULL) {
+		return 1;
+	}
+	for (int i = 0; i < 3; i++) {
+		if (strcmp(args[0], builtins[i]) == 0) {
+			return (*builtins_func[i])(args);
+		}
+	}
+	return sh_launch(args);
 }
 
-int init_prompt(void)
+//=== PROMPT ===================================================================
+
+static inline void init_prompt(prompt_t* prompt)
 {
-        USERNAME = getlogin();
-        if (gethostname(HOSTNAME, sizeof(HOSTNAME)) == -1 || USERNAME == NULL) {
-                fprintf(stderr, "sh: could not access hostname/username");
-                return EXIT_FAILURE;
-        }
-        return EXIT_SUCCESS;
+	prompt -> username = getlogin();
+	if (gethostname(prompt -> hostname, sizeof(prompt -> hostname)) == -1 || prompt -> username == NULL) {
+		fprintf(stderr, "sh: could not access hostname/username");
+		prompt -> init_error = true;
+	}
+	prompt -> init_error = false;
 }
 
 
-inline void draw_prompt(void)
+static inline void draw_prompt(char* username, char* hostname)
 {
-        char cwd[1024];
-        getcwd(cwd, sizeof(cwd));
-        printf_color(
-                1,
-                "[g]\033[1m%s\033[m[/g]:[r][[/r][b]\033[1m%s\033[m[/b][r]][/r]:[m]%s[/m][y]\033[1m$\033[m[/y] ",
-                HOSTNAME,
-                USERNAME,
-                cwd
-        );
+	char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+	printf_color(1, PROMPT, hostname, username, cwd);
 }
 
+//=== MAIN =====================================================================
 
 int main(void)
 {
-        int status;
-        if (init_prompt() == EXIT_FAILURE) {
-                return EXIT_FAILURE;
-        }
-        do {
-                draw_prompt();
-                char* line = sh_read_line();
-                char** args = sh_split_line(line);
-                status = sh_execute(args);
-                free(args);
-                free(line);
-        } while (status);
-        return EXIT_SUCCESS;
+	int status;
+	prompt_t* prompt = (prompt_t*) malloc(sizeof(prompt_t));
+	init_prompt(prompt);
+	if (prompt -> init_error) {
+		free(prompt);
+		return EXIT_FAILURE;
+	}
+	do {
+		draw_prompt(prompt -> username, prompt -> hostname);
+		char* line = sh_read_line();
+		char** args = sh_split_line(line);
+		status = sh_execute(args);
+		free(args);
+		free(line);
+	} while (status);
+	free(prompt);
+	return EXIT_SUCCESS;
 }
--- src/sh.h
+++ src/sh.h
@@ -1,12 +1,35 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-char* sh_read_line(void);
-char** sh_split_line(char* line);
-int sh_launch(char** args);
-int sh_cd(char** args);
-int sh_help(char** args);
-int sh_exit(char** args);
-int sh_execute(char** args);
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define BUFSIZE 64
+#define DELIMITER " \t\r\n\a"
+#define PROMPT "[g]\033[1m%s\033[m[/g]:[r][[/r][b]\033[1m%s\033[m[/b][r]][/r]:[m]%s[/m][y]\033[1m$\033[m[/y] "
+
+#define UNUSED(x) (void) x
+
+typedef struct prompt_t prompt_t;
+struct prompt_t {
+    char* username;
+    char hostname[256];
+    bool init_error;
+};
+
+static char* sh_read_line(void);
+static char** sh_split_line(char* line);
+static int sh_launch(char** args);
+static int sh_cd(char** args);
+static int sh_help(char** args);
+static int sh_exit(char** args);
+static int sh_execute(char** args);
+static inline void init_prompt(prompt_t* prompt);
+static inline void draw_prompt(char* username, char* hostname);
 
 #endif
